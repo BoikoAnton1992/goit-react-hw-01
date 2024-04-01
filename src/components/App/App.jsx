c17a26b7123ddd521bad3a0972fcf911ce7e8372
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-04-01 13:20:48 +0300

    Fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import Profile from '../Profile/Profile';
 import FriendList from '../FriendList/FriendList';
 import TransactionHistory from '../TransactionHistory/TransactionHistory';
-import userData from '../Data/userData.json';
-import friendListData from '../Data/friedListData.json';
-import transactionData from '../Data/transactionData.json';
+import userData from '../../data/userData.json';
+import friendListData from '../../data/friedListData.json';
+import transactionData from '../../data/transactionData.json';
 
 export default function App() {
   return (
